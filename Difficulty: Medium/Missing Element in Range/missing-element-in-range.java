ceca8b07bfda3ae25492f3392840ad36fccc6843
Muditdixit24 <muditdxt24@gmail.com>
2026-02-19 20:10:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Missing Element in Range/missing-element-in-range.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ArrayList<Integer> missingRange(int[] arr, int low, int high) {
+        // code here
+        HashSet<Integer>set = new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            set.add(arr[i]);
+        }
+        ArrayList<Integer>result= new ArrayList<>();
+        for(int i=low;i<=high;i++){
+            if(!set.contains(i)){
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
