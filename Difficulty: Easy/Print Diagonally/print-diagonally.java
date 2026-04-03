842f1bf6e2d3d74f3552959fe810ae02a37cf2a2
Muditdixit24 <muditdxt24@gmail.com>
2026-04-03 18:46:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Diagonally/print-diagonally.java
@@ -0,0 +1,28 @@
+class Solution {
+    static ArrayList<Integer> diagView(int mat[][]) {
+        int n = mat.length;
+        ArrayList<Integer> res = new ArrayList<>();
+
+       
+        for (int col = 0; col < n; col++) {
+            int i = 0, j = col;
+            while (i < n && j >= 0) {
+                res.add(mat[i][j]);
+                i++;
+                j--;
+            }
+        }
+
+        
+        for (int row = 1; row < n; row++) {
+            int i = row, j = n - 1;
+            while (i < n && j >= 0) {
+                res.add(mat[i][j]);
+                i++;
+                j--;
+            }
+        }
+
+        return res;
+    }
+}
