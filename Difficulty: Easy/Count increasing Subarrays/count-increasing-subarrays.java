e07962af924a84a20c5fc56821bc018cb4085713
Muditdixit24 <muditdxt24@gmail.com>
2026-04-12 00:53:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count increasing Subarrays/count-increasing-subarrays.java
+++ Difficulty: Easy/Count increasing Subarrays/count-increasing-subarrays.java
@@ -2,13 +2,13 @@ class Solution {
     public int countIncreasing(int[] arr) {
         // code here
         int n = arr.length;
-        int len = 1;   // current increasing length
+        int len = 1;   
         int ans = 0;
         
         for (int i = 1; i < n; i++) {
             if (arr[i] > arr[i - 1]) {
                 len++;
-                ans += (len - 1); // new subarrays formed
+                ans += (len - 1); 
             } else {
                 len = 1;
             }
