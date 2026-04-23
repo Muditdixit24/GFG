6a073490c3bd6b0b497c91c407f70e1ae4a5102f
Muditdixit24 <muditdxt24@gmail.com>
2026-04-23 18:27:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int cntSubarrays(int[] arr, int k) {
+        // code here
+        HashMap<Integer,Integer> map= new HashMap<>();
+        map.put(0,1);
+        int totalsum=0;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            totalsum+=arr[i];
+            if(map.containsKey(totalsum-k)){
+                count+=map.get(totalsum-k);
+            }
+            map.put(totalsum,map.getOrDefault(totalsum,0)+1);
+        }
+        return count;
+    }
+}
